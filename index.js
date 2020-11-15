fece423089f180a357ee3f96a91fe5992bfe90d5
benboi8 <45974327+benboi8@users.noreply.github.com>
2020-11-15 18:05:52 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,22 +2,78 @@ function CreateBoard(width, height) {
     createCanvas(width, height);
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+	let result = Math.floor(Math.random() * (max - min + 1));
+	return result;
+}
+
+function Round(numToRound, multiplier=25) {
+	return (Math.ceil(numToRound / multiplier) * multiplier) - multiplier;
+}
+
+
 class Food {
-	constructor() {
-		this.posX = Math.round((Math.floor(Math.random() * Math.floor(width - 25))) / 25) * 25;
-		this.posY = Math.round((Math.floor(Math.random() * Math.floor(height - 25))) / 25) * 25;
+	constructor(size=20) {
+		this.size = size;
+		this.posX = 500
+		this.posY = height / 2
 	}
 
-	draw() {
-		stroke('rgb(200, 25, 25)');
-		fill('rgb(200, 25, 25)');
+	draw(index) {
+		stroke('rgb(212, 30, 17)');
+		fill('rgb(212, 30, 17)');
 		strokeWeight(4);
-		square(this.posX + 1, this.posY + 1, 15);
+		square(this.posX, this.posY, this.size);
+		
+		/*
+		textSize(16);
+		fill("white");
+		stroke(0);
+		text(index, this.posX, this.posY);
+		*/
 	}
 	
-	createNewFood() {
-		this.posX = Math.round((Math.floor(Math.random() * Math.floor(width - 25))) / 25) * 25;
-		this.posY = Math.round((Math.floor(Math.random() * Math.floor(height - 25))) / 25) * 25;
+	createNewFood(falseMoves) {
+		let validMove = false;
+		let currentValidMoves = [];
+		while (!validMove) {
+			this.posX = Round(getRandomInt(this.size, width - this.size));
+			this.posY = Round(getRandomInt(this.size, height - this.size));
+			for (let i = 0; i < falseMoves.length; i++) {
+				let headPos = falseMoves[i].split(":");
+				
+				let headPosXString = headPos[0];
+				let headPosYString = headPos[1];
+				
+				let bodyPosX = parseInt(headPosXString);
+				let bodyPosY = parseInt(headPosYString);
+
+				if (this.posX == bodyPosX && this.posY == bodyPosY) {
+					this.posX = Round(getRandomInt(this.size, width - this.size));
+					this.posY = Round(getRandomInt(this.size, height - this.size));
+					currentValidMoves.push(false);
+				}else {
+					currentValidMoves.push(true);
+				}
+			}
+			let isValid = true;
+			for (let i = 0; i < currentValidMoves.length; i++) {
+				if (currentValidMoves[i] == false) {
+					isValid = false;
+					currentValidMoves = [];
+					this.posX = Round(getRandomInt(this.size, width - this.size));
+					this.posY = Round(getRandomInt(this.size, height - this.size));
+				}
+			}
+			if (isValid) {
+				validMove = true;
+			}else {
+				this.posX = Round(getRandomInt(this.size, width - this.size));
+				this.posY = Round(getRandomInt(this.size, height - this.size));
+			}
+		}
 	}
 }
 
@@ -28,22 +84,23 @@ class Snake {
 		this.posX = startX;
 		this.posY = startY;
 		this.dir = dir;
-		this.len = len * 10;
-		this.size = 17
+		this.len = len * speed;
+		this.size = 20;
 		this.piece = 0;
 		this.added = false;
-		this.moveAmount = 2;
+		this.moveAmount = 25;
 		this.moved = false;
 		this.headMoves = [];
-		this.bodyPos = [];
+		this.bodyMoves = [];
 	}
 	
 	drawHead() {
-		fill('rgb(255, 50, 50)');
-		stroke('rgb(255, 50, 50)');
+		fill('rgb(0, 255, 0)');
+		stroke('rgb(0, 255, 0)');
+		strokeWeight(4);
 		
-		this.headPosX = Math.round((Math.floor(this.posX - 25)) / 25) * 25;
-		this.headPosY = Math.round(Math.floor(this.posY - 25) / 25) * 25;
+		this.headPosX = this.posX;
+		this.headPosY = this.posY;
 		square(this.headPosX, this.headPosY, this.size);
 		
 		let headPosXString = this.headPosX.toString();
@@ -59,7 +116,7 @@ class Snake {
 	drawBody() {
 		stroke('rgb(50, 255, 50)');
 		fill('rgb(50, 255, 50)');
-		
+		strokeWeight(4);
 		
 		for (let i = 0; i < this.headMoves.length; i++) {
 			let headPos = this.headMoves[i].split(":");
@@ -76,8 +133,17 @@ class Snake {
 				if (this.headMoves.length >= this.len) {
 					this.headMoves.shift();
 				}
-				
-				this.bodyPos.push(this.bodyPosX, this.bodyPosY);
+				for (let j = 0; j < this.bodyMoves.length; j++) {
+					if (this.bodyMoves[i] != this.headMoves[i]) {
+						this.bodyMoves.shift();
+					}
+				}
+				if (this.bodyPosX != this.headPosX && this.bodyPosY != this.headPosY) {
+					let bodyPosXString = this.bodyPosX.toString();
+					let bodyPosYString = this.bodyPosY.toString();
+					this.bodyMoves.push(bodyPosXString + ":" + bodyPosYString);
+					console.log(this.bodyMoves);
+				}
 			}
 		}
 	}
@@ -96,27 +162,29 @@ class Snake {
 			this.posY -= this.moveAmount;
 			this.moved = true;
 		}
+		
+		
 	}
 	
 	updateDirection() {
 		if ((this.dir == 1 || this.dir == -1) && this.moved) {
-			if (key == "w") {
+			if (key == "w" || keyCode === UP_ARROW) {
 				this.dir = -2;
 				this.moved = false;
 				return;
 
-			}else if (key == "s") {
+			}else if (key == "s" || keyCode === DOWN_ARROW) {
 				this.dir = 2;
 				this.moved = false;
 				return;
 			}
 		}else if ((this.dir == 2 || this.dir == -2) && this.moved) {
-			if (key == "d") {
+			if (key == "d" || keyCode === RIGHT_ARROW) {
 				this.dir = 1;
 				this.moved = false;
 				return;
 				
-			}else if (key == "a") {
+			}else if (key == "a" || keyCode === LEFT_ARROW) {
 				this.dir = -1;
 				this.moved = false;
 				return;
@@ -128,26 +196,27 @@ class Snake {
 		// collide with food
 		let collidedWithFood = false;
 		let collidedWithBorder = false;
+		let collidedWithBody = false;
 		if ((this.headPosX == foodObj.posX) && (this.headPosY == foodObj.posY)) {
 			collidedWithFood = true;
 		}
 
 		if (collidedWithFood) {
-			foodObj.createNewFood();
+			foodObj.createNewFood(this.headMoves);
 			score += 1;
 			updateScore();
-			this.len+=10;
+			this.len+=speed;
 			collidedWithFood = false;
 		}
 		
 		// collide with walls
-		if (this.headPosX - this.size < -17) {
+		if (this.headPosX - this.size < -this.size) {
 			collidedWithBorder = true;
 		}
 		if (this.headPosX >= width) {
 			collidedWithBorder = true;
 		}
-		if (this.headPosY - this.size < -17) {
+		if (this.headPosY - this.size < -this.size) {
 			collidedWithBorder = true;
 		}
 		if (this.headPosY >= height) {
@@ -159,22 +228,70 @@ class Snake {
 		}
 		
 		// collide with body
+		for (let i = 0; i < this.bodyMoves.length; i++) {
+			let bodyPos = this.bodyMoves[i].split(":");
+		
+			let bodyPosXString = bodyPos[0];
+			let bodyPosYString = bodyPos[1];
+			
+			let bodyPosX = parseInt(bodyPosXString);
+			let bodyPosY = parseInt(bodyPosYString);
+		
+			stroke("rgb(0, 0, 255)");
+			fill("rgb(0, 0, 255)")
+			square(bodyPosX + 5, bodyPosY + 5, 10);
+			textSize(16);
+			fill("white");
+			stroke(0);
+			text(i, bodyPosX, bodyPosY);
+			
+			if (this.headPosX == bodyPosX && this.headPosY == bodyPosY) {
+				collidedWithBody = true;
+			}
+		}
+		
+		if (collidedWithBody) {
+			isGameOver = true;
+		}
 	}
 }
 
 
 function gameOver() {
 	textSize(128);
-	fill("white");
-	stroke("black");
+	strokeWeight(6);
+	fill("#cdcdcd");
+	stroke("#202020");
 	text("Game Over", width / 9, height / 1.8);
 	textSize(64);
-	fill("white");
-	stroke("black");
+	fill("#cdcdcd");
+	stroke("#202020");
 	let text_to_render = "Score: " + score; // change to high score
 	text(text_to_render, width / 5, height / 1.4);
+	if (key === "r") {
+		restartGame();
+	}
+}
+
+function restartGame() {
+	isGameOver = false;
+	score = 0;
+	speed = 7;
+
+	allFood = [];
+	numOfFood = 1;
+	for (let i = 0; i < numOfFood; i++) {
+		allFood.push(new Food());
+	}
+
+	snake = new Snake(200, height / 2, 1, 2);
+
+	paused = false;
+	isGameOver = false;
+
+	counter = 0;
 }
 
 function updateScore() {
-	document.getElementById("ScoreCounter").innerHTML = "Score: " + score; 
+	document.getElementById("ScoreCounter").innerHTML = "Score: " + score;
 }
--- sketch.js
+++ sketch.js
@@ -1,18 +1,24 @@
-let width = 800 - 6, height = 600 - 6;
+let width = 800, height = 800;
 
 function setup() {
   CreateBoard(width, height)
 }
 
 let score = 0;
+let speed = 6;
 
-let posX = width/2, posY = height/2;
-let food = new Food();
+let allFood = [];
+let numOfFood = 1;
+for (let i = 0; i < numOfFood; i++) {
+	allFood.push(new Food());
+}
 
-let snake = new Snake(206, (height + 6) / 2, 1, 10);
+let snake = new Snake(200, height / 2, 1, 3);
 
 let paused = false;
 let isGameOver = false;
+
+let counter = 0;
 
 
 function draw() {
@@ -24,17 +30,28 @@ function draw() {
 	if (!isGameOver) {
 		snake.drawBody();
 		
-		food.draw();
+		for (let i = 0; i < allFood.length; i++) {
+			allFood[i].draw(i);
+		}
 		
 		snake.drawHead();
 		
+		
 		if (!paused) {
-			snake.Collide(food);
-			snake.updatePos();	
+			for (let i = 0; i < allFood.length; i++) {
+				snake.Collide(allFood[i]);
+			}
+			if (counter >= speed) {
+				snake.updatePos();	
+				counter = 0;
+			}
+			counter++;
+			
 		}else {
 			textSize(128);
-			fill("white");
-			stroke(0);
+			strokeWeight(6);
+			fill("#cdcdcd");
+			stroke("#202020");
 			text("Paused", width / 4, height / 1.8);
 		}
 	}else {
@@ -45,7 +62,7 @@ function draw() {
 
 function keyPressed() {
 	if (keyCode == ESCAPE) {
-		paused = !paused
+		paused = !paused;
 	}else if(!paused) {
 		snake.updateDirection();
 	}
